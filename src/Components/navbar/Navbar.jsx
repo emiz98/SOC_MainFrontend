e7e8482b4f1baffbef7038abee9d277d36fcaf1f
emiz98 <64089619+emiz98@users.noreply.github.com>
2021-10-03 23:34:04 +0530

    Major Release

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -5,55 +5,102 @@ import {
   SearchOutlined,
 } from "@material-ui/icons";
 import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Button,
+  Nav,
+  Container,
+  NavDropdown,
+  Navbar as Navbars,
+} from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./navbar.scss";
 
 const Navbar = () => {
-  const [isScrolled, setIsScrolled] = useState(false);
-  window.onscroll = () => {
-    setIsScrolled(window.pageYOffset === 0 ? false : true);
-    return () => (window.onscroll = null);
-  };
+  // const [isScrolled, setIsScrolled] = useState(false);
+  // window.onscroll = () => {
+  //   setIsScrolled(window.pageYOffset === 0 ? false : true);
+  //   return () => (window.onscroll = null);
+  // };
 
   return (
-    <div className={isScrolled ? "navbar scrolled" : "navbar"}>
-      <div className="container contain">
-        <div className="left">
-          <Link to="/" className="linking">
+    <Navbars collapseOnSelect expand="lg" variant="dark">
+      <Container>
+        <Navbars.Brand>
+          <Link to="/" className="navbarBrand">
             <img
               src="//upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
               alt=""
             />
           </Link>
-          <Link to="/" className="linking">
-            <span className="linking">Home</span>
-          </Link>
-          <Link to="/cinemas" className="linking">
-            <span className="linking">Cinemas</span>
-          </Link>
-          <Link to="/blogs" className="linking">
-            <span className="linking">Blogs</span>
-          </Link>
-        </div>
+        </Navbars.Brand>
+        <Navbars.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbars.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Link to="/" className="linking">
+              <span className="linking">Home</span>
+            </Link>
 
-        <div className="right">
-          {/* <SearchOutlined className="icon" /> */}
-          <img src="/user.jpg" alt="" />
+            <Link to="/cinemas" className="linking">
+              <span className="linking">Cinemas</span>
+            </Link>
 
-          <div className="profile">
-            <ExpandMore className="icon" />
-
-            <div className="options">
+            <Link to="/blogs" className="linking">
+              <span className="linking">Blogs</span>
+            </Link>
+          </Nav>
+          <Nav>
+            <NavDropdown title="Account" id="collasible-nav-dropdown">
               <Link to="/account" className="linking">
-                <span className="linking">Edit Account</span>
+                <span className="linking rightLinks">Edit Account</span>
               </Link>
+              <NavDropdown.Divider />
+              <Link to="/account" className="linking">
+                <span className="linking rightLinks">Log Out</span>
+              </Link>
+            </NavDropdown>
+          </Nav>
+        </Navbars.Collapse>
+      </Container>
+    </Navbars>
+    // <div className={isScrolled ? "navbar scrolled" : "navbar"}>
+    //   <div className="container contain">
+    //     <div className="left">
+    //       <Link to="/" className="linking">
+    //         <img
+    //           src="//upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
+    //           alt=""
+    //         />
+    //       </Link>
+    //       <Link to="/" className="linking">
+    //         <span className="linking">Home</span>
+    //       </Link>
+    //       <Link to="/cinemas" className="linking">
+    //         <span className="linking">Cinemas</span>
+    //       </Link>
+    //       <Link to="/blogs" className="linking">
+    //         <span className="linking">Blogs</span>
+    //       </Link>
+    //     </div>
 
-              <span>Logout</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    //     <div className="right">
+    //       {/* <SearchOutlined className="icon" /> */}
+    //       <img src="/user.jpg" alt="" />
+
+    //       <div className="profile">
+    //         <ExpandMore className="icon" />
+
+    //         <div className="options">
+    //           <Link to="/account" className="linking">
+    //             <span className="linking">Edit Account</span>
+    //           </Link>
+
+    //           <span>Logout</span>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
